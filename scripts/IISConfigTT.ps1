43ce180dfbd9c810fa81e79add1fcadf7cb2c866
vrebyachih <vrebyachih@baltbet.ru>
2021-11-12 16:23:21 +0300

    add tt logic

--- scripts/IISConfigTT.ps1
+++ scripts/IISConfigTT.ps1
@@ -1,6 +1,6 @@
 import-module '.\scripts\sideFunctions.psm1'
 
-write-host 'config iis for ms' 
+write-host 'config iis for TT' 
 
 ##Credential provided by jenkins
 $username = "$($ENV:SERVICE_CREDS_USR)" 
@@ -9,7 +9,7 @@ $RuntimeVersion ='v4.0'
 $preloader = "SitePreload"
 $IISPools = @( 
     @{
-        SiteName = 'MarketingService'
+        SiteName = 'TradingTool.Api'
         DomainAuth =  @{
             userName="$username";password="$pass";identitytype=3
             }
@@ -18,39 +18,7 @@ $IISPools = @(
                 @{protocol='https';;bindingInformation="*:9880:"}
             )
 		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
-    }
-    @{
-        SiteName = 'MarketingImages'
-        DomainAuth =  @{
-            userName="$username";password="$pass";identitytype=3
-            }
-        Bindings= @(
-                @{protocol='http';bindingInformation="*:8883:"}
-                @{protocol='https';bindingInformation="*:9883:"}
-            )
-		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
-    }
-    @{
-        SiteName = 'MarketingServiceAdmin'
-        DomainAuth =  @{
-            userName="$username";password="$pass";identitytype=3
-            }
-        Bindings= @(
-                @{protocol='http';bindingInformation="*:8881:"}
-                @{protocol='https';bindingInformation="*:9881:"}
-            )
-		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
-    }
-    @{
-        SiteName = 'MarketingServiceClient'
-        DomainAuth =  @{
-            userName="$username";password="$pass";identitytype=3
-            }
-        Bindings= @(
-                @{protocol='http';bindingInformation="*:8882:"}
-                @{protocol='https';bindingInformation="*:9882:"}
-            )
-		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
+		rootDir = ''
     }
 )  
 
@@ -58,14 +26,6 @@ $IISPools = @(
 
 Import-Module -Force WebAdministration
 foreach($site in $IISPools ){
-	RegisterIISSite($site)
+	echo $site
+	#RegisterIISSite($site)
 }
-
-###
-# allowDoubleEscaping
-###
-C:\Windows\system32\inetsrv\AppCmd.exe set config $IISPools[0].SiteName -section:system.webServer/security/requestfiltering -allowDoubleEscaping:true
-###
-# set idleTimeOut 
-###
-Set-ItemProperty ("IIS:\AppPools\$($IISPools[0].SiteName)") -Name processModel.idleTimeout -value ( [TimeSpan]::FromMinutes(1))
