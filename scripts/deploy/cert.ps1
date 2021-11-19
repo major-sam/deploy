39553309a55f9421dca83bd9969a2afb169d7c02
vrebyachih <vrebyachih@baltbet.ru>
2021-11-19 17:03:07 +0300

    security fix

--- scripts/deploy/cert.ps1
+++ scripts/deploy/cert.ps1
@@ -1,18 +1,16 @@
-# Добавляем сертификат в корневое хранилище
+# Добавляем сертификат в корневое хранилище 
+# CERT FILES PROVIDED BY JENKINS
 Write-Host -ForegroundColor Green "[INFO] Add certificate 'gkbaltbet-ca' to Root Trusted"
-#Import-Certificate -FilePath ".\scripts\deploy\gkbaltbet-ca.cer" -CertStoreLocation Cert:\LocalMachine\Root
-certutil.exe -addstore root ".\scripts\deploy\gkbaltbet-ca.cer"
+certutil.exe -addstore root $env:GKBALTBETCA
 Push-Location $env:WORKSPACE
-# Добавляем новый сертификат в Windows(Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606)
-#Write-Host -ForegroundColor Green "[INFO] Add new *.bb-weapps.com certificate"
 $Secure_String_Pwd = ConvertTo-SecureString "BaltbetBB" -AsPlainText -Force
-Import-PfxCertificate -FilePath ".\scripts\deploy\bb-webapps.pfx" -CertStoreLocation Cert:\LocalMachine\My -Password $Secure_String_Pwd
+Import-PfxCertificate -FilePath $env:BBWEBAPPS -CertStoreLocation Cert:\LocalMachine\My -Password $Secure_String_Pwd
 
 Push-Location $env:WORKSPACE
 
 # Добавляем новый сертификат в Windows
 Write-Host -ForegroundColor Green "[INFO] Add new test.wcf.host.pfx certificate"
 $Secure_String_Pwd = ConvertTo-SecureString "12345" -AsPlainText -Force
-Import-PfxCertificate -FilePath ".\scripts\deploy\test.wcf.host.pfx" -CertStoreLocation Cert:\LocalMachine\My -Password $Secure_String_Pwd
+Import-PfxCertificate -FilePath $env:WCFHOST -CertStoreLocation Cert:\LocalMachine\My -Password $Secure_String_Pwd
 
 Push-Location $env:WORKSPACE
--- scripts/deploy/tt/cert.ps1
+++ scripts/deploy/tt/cert.ps1
@@ -1,5 +1,4 @@
 # Добавляем сертификат в корневое хранилище
 Write-Host "[INFO] Add certificate 'adfs' to Root Trusted"
-#Import-Certificate -FilePath ".\scripts\deploy\gkbaltbet-ca.cer" -CertStoreLocation Cert:\LocalMachine\Root
-certutil.exe -addstore root ".\scripts\deploy\tt\adfs.cer"
-certutil.exe -addstore -f "My" ".\scripts\deploy\tt\adfs.cer"
+certutil.exe -addstore root  $env:TTADFS
+certutil.exe -addstore -f "My" $env:TTADFS
