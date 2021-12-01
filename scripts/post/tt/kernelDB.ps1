aa5a8a355c7c70351bcea5ef924ccacb68c260fc
vrebyachih <vrebyachih@baltbet.ru>
2021-12-01 17:43:57 +0300

    fix import query

--- scripts/post/tt/kernelDB.ps1
+++ scripts/post/tt/kernelDB.ps1
@@ -33,13 +33,5 @@ else{    $conn = New-Object Microsoft.SqlServer.Management.Common.ServerConnecti
     write-host "done"    
 }
 
-$passVar = ConvertTo-SecureString $ENV:TT_SERVICE_CREDS_PSW -AsPlainText -Force
-$credentials = New-Object System.Management.Automation.PSCredential ($ENV:TT_SERVICE_CREDS_USR , $passVar )
-$scriptBlock = { importsystemmodules; invoke-sqlcmd -QueryTimeout 720 -database 'BaltbetM'  -ServerInstance LOCALHOST -InputFile  ".\scripts\post\tt\KernelLineEvents.sql" }
-$job = Start-Job  -ScriptBlock $scriptBlock -Credential $credentials |get-job |Wait-job
-$result = Receive-Job -Job $job
-$result
-
-Remove-Job -Job $job
-
+invoke-sqlcmd -QueryTimeout 720 -database 'BaltbetM'  -ServerInstance LOCALHOST -InputFile  ".\scripts\post\tt\KernelLineEvents.sql" 
 
