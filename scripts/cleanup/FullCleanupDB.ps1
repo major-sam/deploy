2d21ec6c500138aac7e31d431a50b2f6a54af7b3
vrebyachih <vrebyachih@baltbet.ru>
2021-12-09 16:13:50 +0300

    fixcreds

--- scripts/cleanup/FullCleanupDB.ps1
+++ scripts/cleanup/FullCleanupDB.ps1
@@ -10,11 +10,5 @@ $rQuery =" EXEC sp_MSforeachdb
     EXEC (''ALTER DATABASE [?] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE [?]'' )
   END'
-  USE [msdb]
-  GO
-  DECLARE @Delete_Date [datetime]
-  SET @Delete_Date = GETDATE()
-  exec sp_delete_backuphistory @Delete_Date 
-  GO
   "
 invoke-sqlcmd -ServerInstance $env:computername -Query $rQuery
