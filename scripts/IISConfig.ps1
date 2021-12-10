9367c97bbbc082304d19157a7d67b313943ec044
vrebyachih <vrebyachih@baltbet.ru>
2021-12-10 15:57:03 +0300

    add baltbetcom

--- scripts/IISConfig.ps1
+++ scripts/IISConfig.ps1
@@ -17,8 +17,8 @@ $IISPools = @(
                 @{protocol='https';bindingInformation="*:4444:"}
             )
 		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
-		rootDir = 'c:\inetpub'
-		siteSubDir = $true
+		rootDir = 'c:\inetpub\WebsiteCom'
+		siteSubDir = $false
     }
     @{
         SiteName = 'ClientWorkSpace'
