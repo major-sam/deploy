55753fae347fb8ec699445d5352b8dffe8fae932
vrebyachih <vrebyachih@baltbet.ru>
2021-12-13 19:20:46 +0300

    disable post cis

--- scripts/deploy/UniPaymentsService.ps1
+++ scripts/deploy/UniPaymentsService.ps1
@@ -11,16 +11,6 @@ $source = GetSourceObject $sourceparams
 
 $targetDir = "C:\inetpub\Uni.PaymentsService"
 
-# Копируем сервис
-Write-Host -ForegroundColor Green "[INFO] Copy Uni.PaymentsService"
-
-write-host "Copy-Item -Path $($source.sourceBuildSource)  -Destination $targetDir -Recurse "
-robocopy "$($source.sourceBuildSource)"  $targetDir /e /NFL /NDL /nc /ns /np
-$global:LASTEXITCODE
-
-if ($global:LASTEXITCODE -ne 0){
-	$global:LASTEXITCODE = 0
-}
 Write-Host -ForegroundColor Green "[INFO] Change settings $targetDir\appsettings.json"
 $ConfigPath = "$targetDir\appsettings.json"
 $new_host = $env:COMPUTERNAME
