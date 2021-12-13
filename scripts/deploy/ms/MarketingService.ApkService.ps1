6b2f875973bb1dda2b315bb03338455e5d629e46
vrebyachih <vrebyachih@baltbet.ru>
2021-12-13 17:28:41 +0300

    fix marketingg log & cert

--- scripts/deploy/ms/MarketingService.ApkService.ps1
+++ scripts/deploy/ms/MarketingService.ApkService.ps1
@@ -10,6 +10,13 @@ $CurrentIpAddr =(Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScr
 $conf = [Xml](Get-Content $webConfig)
 $conf.configuration."system.serviceModel".services.service |% {$_.endpoint |% {$_.address = $_.address.replace("localhost",$CurrentIpAddr)}}
 
+$conf.configuration.log4net.appender.file.value = 'c:\logs\Marketing.ApkService\'
+$conf.configuration."system.serviceModel".behaviors.serviceBehaviors.behavior |% {
+	    if ($_.name -like "wcfSecureServiceBehavior"){
+			        $_.serviceCredentials.serviceCertificate.findValue = "test.wcf.host"
+			}
+	}
+
 $conf.configuration.connectionStrings.add |Where-Object  name -eq "BaltBetM"| %{
 	$_.connectionString = 'server=localhost;Integrated Security=SSPI;MultipleActiveResultSets=true;Initial Catalog=BaltBetM'}
 $conf.Save($webConfig)
