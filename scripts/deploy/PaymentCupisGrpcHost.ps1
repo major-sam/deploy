7722a4904939aca452c2b0513263adb6c3203478
syudin <syudin@baltbet.ru>
2021-12-14 16:56:31 +0300

    Modify PaymentCupisGrpcHost.ps1

--- scripts/deploy/PaymentCupisGrpcHost.ps1
+++ scripts/deploy/PaymentCupisGrpcHost.ps1
@@ -1,3 +1,5 @@
+Import-module '.\scripts\sideFunctions.psm1'
+
 # Создаем БД Cupis.GrpcHost
 $dbname = "Cupis.GrpcHost"
 
@@ -9,18 +11,23 @@ Invoke-sqlcmd -ServerInstance $env:COMPUTERNAME -Query "CREATE DATABASE [$dbname
 $ServiceName = "PaymentCupisGrpcHost"
 $ServiceFolderPath = "C:\Services\${ServiceName}"
 $DataSource = "localhost"
-$IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1 }).IPAddress.trim()
+$IPAddress = (Get-NetIPAddress -AddressFamily ipv4 | Where-Object -FilterScript { $_.interfaceindex -ne 1 }).IPAddress.trim()
 
 Write-Host -ForegroundColor Green "[INFO] Edit BaltBet.Payment.Cupis.GrpcHost configuration files..."
-$config = Get-Content -Path "$ServiceFolderPath\appsettings.json" -Encoding UTF8 | ConvertFrom-Json
+$config = Get-Content -Path "$ServiceFolderPath\appsettings.json" -Encoding UTF8
+$config = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
 $config.DbOptions.ConnectionString = "data source=${DataSource};initial catalog=Cupis.GrpcHost;Integrated Security=true;MultipleActiveResultSets=True;"
 $config.RabbitBusOptions.ConnectionString = "host=localhost"
-$config.AggregatorWcfSettings.NotificationUrl = "http://${IPAddress}:5001/api/v1/notifications/aggregator"
+$config.Serilog.WriteTo[1].Args.path = "C:\logs\Payment.Cupis\Payment.Cupis-.log"
+$config.Kestrel.Endpoints.HttpGrpc.Url = "http://0.0.0.0:5003"
+$config.Kestrel.Endpoints.HttpWeb.Url = "http://0.0.0.0:5002"
+$config.AggregatorGrpcOptions.ServiceAddress = "http://${IPAddress}:32421"
+$config.AggregatorGrpcOptions.NotificationUrl = "http://${IPAddress}:5001/api/v1/notifications/aggregator"
+$config.AggregatorGrpcOptions.CheckWithdrawUrl = "http://${IPAddress}:5001//api/v1/payout/checkwithdraw"
 Set-Content -Path "$ServiceFolderPath\appsettings.json" -Encoding UTF8 -Value ($config | ConvertTo-Json -Depth 100)
 
 
 # Регистрируем сервис
-Import-module '.\scripts\sideFunctions.psm1'
 $serviceBin = Get-Item  "C:\Services\${ServiceName}\BaltBet.Payment.Cupis.GrpcHost.exe"
 $sname = RegisterWinService($serviceBin)
 Start-Service $sname
