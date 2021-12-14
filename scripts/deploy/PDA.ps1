47b316cd3ab31ac4ee94f7187993c7dd5c276edb
vrebyachih <vrebyachih@baltbet.ru>
2021-12-14 13:58:42 +0300

    fix path

--- scripts/deploy/PDA.ps1
+++ scripts/deploy/PDA.ps1
@@ -6,4 +6,4 @@ import-module '.\scripts\sideFunctions.psm1'
 $WebPda_folder = "C:\inetpub\baltplaymobile"
 
 Write-Host -ForegroundColor Green "[INFO] Expand archive WebPDA"
-Expand-Archive -LiteralPath "\\\\server\\tcbuild$\\WebPda\\2021-10-11.master.1024.f463408c.zip" -DestinationPath $WebPda_folder -Force
+Expand-Archive -LiteralPath "\\server\tcbuild$\WebPda\2021-10-11.master.1024.f463408c.zip" -DestinationPath $WebPda_folder -Force
