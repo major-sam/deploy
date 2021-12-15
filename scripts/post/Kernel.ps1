e9c3c6c9b539cbd2c040c53945987abf22f9ad76
vrebyachih <vrebyachih@baltbet.ru>
2021-12-15 17:59:17 +0300

    add kernel sql task & memurai restart

--- scripts/post/Kernel.ps1
+++ scripts/post/Kernel.ps1
@@ -10,3 +10,11 @@ start-Service $sname
 Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
  return 0
 New-SmbShare -Name 'BaltbetLogs' -path C:\Logs
+if (test-path "c:\kernel\TasksDB"){
+	write-host "Exec tasks"
+	get-ChildItem "c:\kernel\taskdb\" -include "*.sql" | % {
+		Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database 'BaltBetM' -InputFile $_ -Verbose -ErrorAction continue
+		}
+}else{
+	write-host "no task for this branch"
+}
