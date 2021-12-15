3d7188c07c58782720507552cd136e437eed773d
syudin <syudin@baltbet.ru>
2021-12-15 15:37:21 +0300

    Add migration scripts for PaymentCupisGrpcHost

---
+++ scripts/deploy/paymentCupisGrpcSql/A002_Release_30092021.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [PaymentsAdditionalData] ADD [PaymentPreviewDataJson] nvarchar(max) NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210930100435_2', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
