0d1fee118d84fe4e2228bf25bb6dafe2e0c818ed
enesudimov <enesudimov@baltbet.ru>
2021-12-15 19:32:18 +0300

    Add WebTouch deploy process

--- scripts/deploy/WebMobile.ps1
+++ scripts/deploy/WebMobile.ps1
@@ -1,19 +1,19 @@
 import-module '..\scripts\sideFunctions.psm1'
 
-# 3 Сервис PDA
+# 3 Сервис WebMobile
 
 #get release params
-$WebMobileFolder = "C:\inetpub\WebMobile"
-$WebConfig = "$WebMobileFolder\Web.config"
+$SiteFolder = "C:\inetpub\WebMobile"
+$SiteConfig = "$SiteFolder\Web.config"
 $CurrentIpAddr =(Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
 
 # Change Web.config
 Write-Host -ForegroundColor Green "[INFO] Edit web.config of WebMobile"
-$webdoc = [Xml](Get-Content $WebConfig)
+$webdoc = [Xml](Get-Content $SiteConfig)
 $MobileServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "MobileServerAddress"
 $MobileServerAddress.value = "$($CurrentIpAddr):8082"
 $SiteServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "SiteServerAddress"
 $SiteServerAddress.value = "$($CurrentIpAddr):8088"
 $ServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "ServerAddress"
 $ServerAddress.value = "$($CurrentIpAddr):8082"
-$webdoc.Save($WebConfig)
+$webdoc.Save($SiteConfig)
--- scripts/deploy/WebTouch.ps1
+++ scripts/deploy/WebTouch.ps1
@@ -3,5 +3,13 @@ import-module '.\scripts\sideFunctions.psm1'
 # 2 Сервис WebTouch
 
 #get release params
+$SiteFolder = "C:\inetpub\WebTouch"
+$SiteConfig = "$SiteFolder\Web.config"
+$CurrentIpAddr =(Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
 
-Write-Host "[INFO] Nothing to do"
+Write-Host -ForegroundColor Green "[INFO] Edit web.config of WebTouch"
+$webdoc = [Xml](Get-Content $SiteConfig)
+$webdoc.configuration.serverConfig.ServerAddress = "$($CurrentIpAddr):8082"
+$webdoc.configuration.serverConfig.SiteServerAddress = "$($CurrentIpAddr):8088"
+$webdoc.Save($SiteConfig)
+
