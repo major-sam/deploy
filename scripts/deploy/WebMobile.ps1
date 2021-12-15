81a3897b19463577cc3a48fcc6a20550295c91cf
enesudimov <enesudimov@baltbet.ru>
2021-12-15 18:09:46 +0300

    Delete config transformation process

--- scripts/deploy/WebMobile.ps1
+++ scripts/deploy/WebMobile.ps1
@@ -7,13 +7,7 @@ $WebMobileFolder = "C:\inetpub\WebMobile"
 $WebConfig = "$WebMobileFolder\Web.config"
 $CurrentIpAddr =(Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
 
-
-$transformationsList = Get-Item -Path "$WebMobileFolder\*" -Include "Web.*.config" -Exclude "*Debug*","*Production*"
-foreach ($transformation in $transformationsList) { 
-    Write-Host -ForegroundColor Green "[INFO] Deploy transformation $transformation to Web.config..."
-    XmlDocTransform($WebConfig, $transformation.FullName.trim()) 
-}
-
+# Change Web.config
 Write-Host -ForegroundColor Green "[INFO] Edit web.config of WebMobile"
 $webdoc = [Xml](Get-Content $WebConfig)
 $MobileServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "MobileServerAddress"
