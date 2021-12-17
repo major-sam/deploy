17409594d20f9dca81a31af7620d27db969dbfcd
vrebyachih <vrebyachih@baltbet.ru>
2021-12-17 16:42:36 +0300

    fix kernel

--- scripts/post/Kernel.ps1
+++ scripts/post/Kernel.ps1
@@ -1,20 +1,5 @@
 import-module '.\scripts\sideFunctions.psm1'
 
-<<<<<<< HEAD
-=======
-$serviceBin = Get-Item  "C:\Kernel\Kernel.exe"
-$sname = RegisterWinService($serviceBin)
-start-Service $sname
-Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
-$serviceBin = Get-Item  "C:\KernelWeb\KernelWeb.exe"
-$sname = RegisterWinService($serviceBin)
-start-Service $sname
-Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
- return 0
-<<<<<<< Updated upstream
-=======
->>>>>>> master
->>>>>>> Stashed changes
 if (test-path "c:\kernel\TasksDB"){
 	write-host "Exec tasks"
 	get-ChildItem "c:\kernel\taskdb\" -include "*.sql" | % {
