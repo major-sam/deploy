6881f4278d7b9a6d022a540effa373fcee379cb9
syudin <syudin@baltbet.ru>
2021-12-17 09:23:11 +0300

    Add ActionLogService

---
+++ scripts/post/ActionLogService.ps1
@@ -0,0 +1,6 @@
+# Регистрируем сервис
+$ServiceName = "ActionLogService"
+$serviceBin = Get-Item  "C:\Services\${ServiceName}\ActionLogService.exe"
+$sname = RegisterWinService($serviceBin)
+Start-Service $sname
+Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
