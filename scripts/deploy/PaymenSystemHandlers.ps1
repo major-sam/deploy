b46f0465a2a655859279c48bd8d582e51e36a50b
vrebyachih <vrebyachih@baltbet.ru>
2021-12-17 19:01:32 +0300

    fix serverip

--- scripts/deploy/PaymenSystemHandlers.ps1
+++ scripts/deploy/PaymenSystemHandlers.ps1
@@ -1,3 +1,7 @@
+$IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1 }).IPAddress.trim()
 $webdoc = [Xml](Get-Content $webConfig)
 $webdoc.configuration.log4net.appender.file.value = "c:\logs\Payments\Payment-handlers\"
+$webdoc.configuration.appSettings.add| %{if ($_.key -like "ServerAddress"){
+											value =	"$($IPAddress):8082"}
+											}
 $webdoc.Save($webConfig)
