e5ec4116c309b1614322c6e4573745ab45d0aea9
syudin <syudin@baltbet.ru>
2021-12-20 12:09:35 +0300

    Get content COMAccounts.sql to $query

--- scripts/post/ComAccounts.ps1
+++ scripts/post/ComAccounts.ps1
@@ -3,6 +3,11 @@
 
 $file = "$($env:workspace)\scripts\post\ComAccountsSql\COMAccounts.sql"
 $queryTimeout = 720
+$MyUrl = "$env:COMPUTERNAME"
 
+# Меняем $query без сохранения оригинала
+$query = (get-content -raw -Encoding UTF8 -Path "$file") -replace '#VM_URL', $MyUrl
+
+# Выполняем полученный $query
 Write-Host -ForegroundColor Green "[INFO] Execute COMAccounts.sql"
-Invoke-Sqlcmd -verbose -QueryTimeout $queryTimeout -ServerInstance $env:COMPUTERNAME -InputFile $file -ErrorAction continue
+Invoke-Sqlcmd -verbose -QueryTimeout $queryTimeout -ServerInstance $env:COMPUTERNAME -Query $query -ErrorAction continue
