55ffbc8996586312e3358239ad15dc732a3aca9e
enesudimov <enesudimov@baltbet.ru>
2021-12-23 12:12:58 +0300

    Add origins to UniPaymentsService.ps1

--- scripts/deploy/UniPaymentsService.ps1
+++ scripts/deploy/UniPaymentsService.ps1
@@ -13,4 +13,7 @@ $new_url = $new_host
 $config = Get-Content -Raw -path $ConfigPath 
 $json_appsetings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
 $json_appsetings.ConnectionStrings.UniSiteSettings = "data source=localhost;initial catalog=UniRu;Integrated Security=SSPI;MultipleActiveResultSets=True;"
+$json_appsetting.Origins[0] = "https://$($new_host).bb-webapps.com:4443"
+$json_appsetting.Origins[1] = "https://$($new_host).bb-webapps.com:4444"
+$json_appsetting.Origins[2] = "https://$($new_host).bb-webapps.com:4445"
 ConvertTo-Json $json_appsetings -Depth 4 | Format-Json | Set-Content $pathtojson -Encoding UTF8
