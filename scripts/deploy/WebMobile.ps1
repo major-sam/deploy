3efbc71f8e9186dadb068d11a2b47da0382d6e3a
enesudimov <enesudimov@baltbet.ru>
2021-12-23 11:37:44 +0300

    Edit WebMobile config

--- scripts/deploy/WebMobile.ps1
+++ scripts/deploy/WebMobile.ps1
@@ -10,10 +10,16 @@ $CurrentIpAddr =(Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScr
 # Change Web.config
 Write-Host -ForegroundColor Green "[INFO] Edit web.config of WebMobile"
 $webdoc = [Xml](Get-Content $SiteConfig)
-$MobileServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "MobileServerAddress"
-$MobileServerAddress.value = "$($CurrentIpAddr):8082"
-$SiteServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "SiteServerAddress"
-$SiteServerAddress.value = "$($CurrentIpAddr):8088"
-$ServerAddress = $webdoc.configuration.appSettings.add | Where-Object key -eq "ServerAddress"
-$ServerAddress.value = "$($CurrentIpAddr):8082"
+$webdoc.configuration.appSettings.add | % { if ($_.key -eq "MobileServerAddress") {  
+        $_.value = "$($CurrentIpAddr):8082"
+    }
+}
+$webdoc.configuration.appSettings.add | % { if ($_.key -eq "SiteServerAddress") {  
+        $_.value = "$($CurrentIpAddr):8088"
+    }
+}
+$webdoc.configuration.appSettings.add | % { if ($_.key -eq "ServerAddress") {  
+        $_.value = "$($CurrentIpAddr):8082"
+    }
+}
 $webdoc.Save($SiteConfig)
