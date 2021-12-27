efecb1ff12bfacbc6264f5dcd95d8d8e17048445
enesudimov <enesudimov@baltbet.ru>
2021-12-27 11:33:01 +0300

    Change grpc port

--- scripts/deploy/UniPaymentsService.ps1
+++ scripts/deploy/UniPaymentsService.ps1
@@ -14,7 +14,7 @@ $json_appsettings.Origins[0] = "https://$($new_host).bb-webapps.com:4443"
 $json_appsettings.Origins[1] = "https://$($new_host).bb-webapps.com:4444"
 $json_appsettings.Origins[2] = "https://$($new_host).bb-webapps.com:4445"
 $json_appsettings.Grpc.Services.Host = "$($new_host)"
-$json_appsettings.Grpc.Services[0].Port = "5010"
+$json_appsettings.Grpc.Services[0].Port = "5003"
 
 ConvertTo-Json $json_appsetings -Depth 4 | Format-Json | Set-Content $ConfigPath -Encoding UTF8
 
