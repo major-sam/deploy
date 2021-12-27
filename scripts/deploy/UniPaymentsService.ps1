a2d504f5b5413fe0da4bc2e7b6f54350ba80eef5
enesudimov <enesudimov@baltbet.ru>
2021-12-27 12:14:09 +0300

    Delete one origins connection string

--- scripts/deploy/UniPaymentsService.ps1
+++ scripts/deploy/UniPaymentsService.ps1
@@ -11,9 +11,8 @@ $config = Get-Content -Raw -path $ConfigPath
 $json_appsettings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
 $json_appsettings.ConnectionStrings.UniSiteSettings = "data source=localhost;initial catalog=UniRu;Integrated Security=True;MultipleActiveResultSets=True;"
 $json_appsettings.Origins[0] = "https://$($new_host).bb-webapps.com:4443"
-$json_appsettings.Origins[1] = "https://$($new_host).bb-webapps.com:4444"
-$json_appsettings.Origins[2] = "https://$($new_host).bb-webapps.com:4445"
-$json_appsettings.Grpc.Services.Host = "$($new_host)"
+$json_appsettings.Origins[1] = "https://$($new_host).bb-webapps.com:4445"
+$json_appsettings.Grpc.Services.Host = $new_host
 $json_appsettings.Grpc.Services[0].Port = 5003
 
 ConvertTo-Json $json_appsetings -Depth 4 | Format-Json | Set-Content $ConfigPath -Encoding UTF8
