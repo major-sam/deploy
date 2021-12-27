c14e051e322c0c9889a997aaafbb3ea670ffcf25
enesudimov <enesudimov@baltbet.ru>
2021-12-27 10:53:06 +0300

    fix UniPayments

--- scripts/deploy/UniPaymentsService.ps1
+++ scripts/deploy/UniPaymentsService.ps1
@@ -7,13 +7,14 @@ $targetDir = "C:\Services\UniPaymentsService"
 Write-Host -ForegroundColor Green "[INFO] Change settings $targetDir\appsettings.json"
 $ConfigPath = "$targetDir\appsettings.json"
 $new_host = $env:COMPUTERNAME
-$old_url = 'vm5-p3'
-$new_url = $new_host
-((Get-Content -Encoding UTF8 -LiteralPath $ConfigPath) -replace $old_url,$new_url) | Set-Content -Encoding UTF8 -LiteralPath $ConfigPath
 $config = Get-Content -Raw -path $ConfigPath 
-$json_appsetings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
-$json_appsetings.ConnectionStrings.UniSiteSettings = "data source=localhost;initial catalog=UniRu;Integrated Security=SSPI;MultipleActiveResultSets=True;"
-$json_appsetting.Origins[0] = "https://$($new_host).bb-webapps.com:4443"
-$json_appsetting.Origins[1] = "https://$($new_host).bb-webapps.com:4444"
-$json_appsetting.Origins[2] = "https://$($new_host).bb-webapps.com:4445"
+$json_appsettings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
+$json_appsettings.ConnectionStrings.UniSiteSettings = "data source=localhost;initial catalog=UniRu;Integrated Security=True;MultipleActiveResultSets=True;"
+$json_appsettings.Origins[0] = "https://$($new_host).bb-webapps.com:4443"
+$json_appsettings.Origins[1] = "https://$($new_host).bb-webapps.com:4444"
+$json_appsettings.Origins[2] = "https://$($new_host).bb-webapps.com:4445"
+$json_appsettings.Grpc.Services.Host = "$($new_host)"
+$json_appsettings.Grpc.Services[0].Port = "5010"
+
 ConvertTo-Json $json_appsetings -Depth 4 | Format-Json | Set-Content $pathtojson -Encoding UTF8
+
