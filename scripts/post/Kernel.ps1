859ceae2988d375e7844101101b474614c970ba6
syudin <syudin@baltbet.ru>
2022-01-13 16:14:24 +0300

    Add logging for kernel post stage

--- scripts/post/Kernel.ps1
+++ scripts/post/Kernel.ps1
@@ -3,11 +3,13 @@ import-module '.\scripts\sideFunctions.psm1'
 if (test-path "c:\kernel\TasksDB"){
 	write-host "Exec tasks"
 	get-ChildItem "c:\Kernel\TasksDB\" -include "*.sql" | % {
+		Write-Host -ForegroundColor Green "[INFO] Execute script $_ on database BaltBetM"
 		Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database 'BaltBetM' -InputFile $_ -Verbose -ErrorAction continue
+		Write-Host -ForegroundColor Green "[INFO] Execute script $_ on database BaltBetMMirror"
 		Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database 'BaltBetMMirror' -InputFile $_ -Verbose -ErrorAction continue
 		}
 }else{
-	write-host "no task for this branch"
+	write-host "[INFO] No tasks for this branch"
 }
 
 #ru&com connection
