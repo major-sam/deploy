1d130335685a39aac04da2da91bd110b815a0a62
enesudimov <enesudimov@baltbet.ru>
2022-01-14 12:11:10 +0300

    Add WepParser deployment process

---
+++ scripts/deploy/webparser/webparser.ps1
@@ -0,0 +1,31 @@
+import-module '.\scripts\sideFunctions.psm1'
+
+$ServicesFolder = "C:\Services"
+$ServiceName = "WebParser"
+$PathToConfig = "$($ServicesFolder)\$($ServiceName)\Config"
+$PathToExeConfig = "$($ServicesFolder)\$($ServiceName)\WebParser.exe.config"
+
+Write-Host "[INFO] EDIT WebParser.exe.config..."
+[xml]$config = Get-Content -Path $PathToExeConfig
+$ServiceWebParser = $config.configuration.'system.serviceModel'.services.service | Where-Object name -eq "WebParser.ServiceWebParser"
+$ServiceWebParser.endpoint.address = "http://$($env:COMPUTERNAME):19020"
+
+$BaseParserContext = $config.configuration.connectionStrings.add | Where-Object name -eq "Parser.Base.Line.ParserContext"
+$BaseParserContext.connectionString = "data source=$($env:COMPUTERNAME);Integrated Security=SSPI;initial catalog=$($ServiceName);MultipleActiveResultSets=True;"
+
+$RabbitConnection = $config.configuration.connectionStrings.add | Where-Object name -eq "RabbitConnection"
+$RabbitConnection.connectionString = "host=$($env:COMPUTERNAME):5672; username=test; password=test; publisherConfirms=true; timeout=100; requestedHeartbeat=0"
+
+$RabbitClientAPK = $config.configuration.connectionStrings.add | Where-Object name -eq "RabbitClientAPK"
+$RabbitClientAPK.connectionString = "host=$($env:COMPUTERNAME):5672; username=test; password=test; publisherConfirms=true; timeout=100; requestedHeartbeat=0"
+
+$config.Save($PathToExeConfig)
+
+
+Write-Host "[INFO] Run XML Transformation"
+XmlDocTransform("$PathToConfig\Settings.xml", "$PathToConfig\Settings.Test.xml")
+
+Write-Host "[INFO] EDIT Settings.xml..."
+[xml]$config = Get-Content -Path "$($PathToConfig)\Settings.xml"
+$config.Settings.GrpcHost = $env:COMPUTERNAME
+$config.Save("$($PathToConfig)\Settings.xml")
